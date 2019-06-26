b54cf265e2764d6cbdf1ba9e09e85f7b8f70a2f2
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-06-26 18:00:48 +0100

    Working on responsive layouts

--- src/templates/posts.js
+++ src/templates/posts.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import Link from "gatsby-link"
 import PropTypes from "prop-types"
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
 //Components
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -32,25 +33,21 @@ class PostsTemplate extends Component {
                         textAlign: "center",
                     }}
                 >{data.allWordpressPost.edges.length} POSTS FOUND</p>
-                <div
-                    className="postList"
-                    style={{
-                        flexWrap: "wrap !important",
-                        flexFlow: "flex-wrap",
-                        display: "flex"
-                    }}
-                >
-                    {data.allWordpressPost.edges.map(({ node }) => (
-                        <div key={node.slug} className="card" id="post" style={{ marginBottom: 50 }}>
-                            <Link to={'thought/' + node.slug}>
-                                <p id="wpPostDate">{node.date}</p>
-                                <h3 className="wpPostTitle">{node.title}</h3>
+                <div className="postList">
+                    <Grid container>
+                        {data.allWordpressPost.edges.map(({ node }) => (<Grid xs={12} md={4}>
+                            <Card key={node.slug} className="card" id="post">
+                                <Link to={'thought/' + node.slug}>
+                                    <p id="wpPostDate">{node.date}</p>
+                                    <h3 className="wpPostTitle">{node.title}</h3>
 
-                                <div className="paraText" dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-                                <img className="featuredWPImage" alt={node.title} src={node.featured_media.source_url} />
-                            </Link>
-                        </div>
-                    ))}
+                                    <div className="paraText" dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+                                    <img className="featuredWPImage" alt={node.title} src={node.featured_media.source_url} />
+                                </Link>
+                            </Card>
+                        </Grid>
+                        ))}
+                    </Grid>
                 </div>
 
             </Layout>
