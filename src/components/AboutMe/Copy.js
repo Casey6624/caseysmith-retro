61ab993855a33c2055be337d533b329e351eeea7
Casey Smith <casey@jollyit.co.uk>
2019-08-13 16:36:20 +0100

    Changed Copy.js which alters the text on the homepage

--- src/components/AboutMe/Copy.js
+++ src/components/AboutMe/Copy.js
@@ -6,43 +6,45 @@ copy.overview = [
     "I write the majority of my projects in C# & JavaScript. ",
     <Fragment>
         <p>I am a final year student from Sheffield who loves to make new things. I am currently enrolled at Sheffield Hallam where I am studying towards a BSc in Computing (Web Development).</p>
-        <p>I am a huge Ice Hockey fan 🏒 (Toronto Maple Leafs 🍁)</p>
+        <p>I am a huge Ice Hockey fan 🏒 and I religiously follow the Toronto Maple Leafs 🍁 </p>
     </Fragment>,
     <Fragment>
         <strong>OSS</strong>
         <p>I am passionate about open source software and copyleft ideology. Open source brings the best minds together and is unrivalled when it comes to increasing the speed and preformance of public projects. </p>
         <strong>Usability</strong>
-        <p>I love making things easy to do for the end user. I try to design all of my projects with </p>
+        <p>I love making things easy to do for the end user. I constantly try to make all of my projects intuative and easy to navigate with the highest of priority.</p>
     </Fragment>
 ]
 
 copy.moreInfo = [
     <Fragment>
-        <p>React is my go to Front-End tool for practically everything. I 💜 its versatility, community and rapid preformance 🚄.I am familair with lots of different React libraries. To name a few:.</p>
-        <li>React Native</li>
-        <li>GatsbyJS</li>
-        <li>Electron</li>
+        <p>React is my go to Front-End tool for practically everything. I 💜 its versatility, community and rapid preformance 🚄.I am familair with a bunch of different React libraries. To name a few in particular:.</p>
+        <p>💛 React Native</p>
+        <p>💚 GatsbyJS</p>
+        <p>💜 Electron</p>
     </Fragment>
     ,
     <Fragment>
         <p>NodeJS is usually my tool of choice whenever a project requires a Back-End. I love its asynchronous by nature attitude and the little time it takes to prototype ideas.</p>
+        <p>I love ExpressJS, but have played around with other frameworks.</p>
     </Fragment>,
     <Fragment>
         <p>.Net is something I have adopted relatively recently. The typed language of C# is a nice refresher to JavaScript and is a great tool for creating sturdy APIs.</p>
-        <br /><p> I am familiar with creating both MVC (Razor .cshtml) Web Apps and Web API</p>
+        <p> I am familiar with creating both MVC (Razor .cshtml) Full-Stack Web Apps and ASP.Net Web APIs</p>
     </Fragment>,
     <Fragment>
         <p>Originating from a WordPress background, I am fairly well versed in PHP, however I don't use it as much these days. I have previous experience developing bespoke WordPress plugins and custom Gutenberg blocks.</p>
     </Fragment>,
     <Fragment>
-        <p>Unlike the other areas in my tech stack, databases are where I am the most flexible. I enjoy working with both NoSQL (MongoDB) and traditional SQL databases.</p>
+        <p>Databases are the part of my tech stack which I am the most flexible about - I enjoy working with both NoSQL (MongoDB) and traditional SQL databases.</p>
+        <p>Recently I have started to adopt NoSQL databases more, purely due to the speed of spinning up and prototyping ideas.</p>
     </Fragment>,
     <Fragment>
         <strong>COMMON TOOLS</strong>
-        <p>Git, Bash, Putty (connecting to VPS), VueJS, GraphQL, Grails, Android Studio, Linux, PowerShell, Office 365, Azure AD, Active Directory, IIS, SSL Certificates, Hosting Apache, Nginx, Bootstrap, Material UI</p>
+        <p>Git, Bash, Putty, VueJS, GraphQL, Grails, Android Studio, Linux, PowerShell, Office 365, Azure AD, Active Directory, IIS, SSL Certificates, Apache, Nginx, Bootstrap, Material UI</p>
         <strong>CERTIFICATIONS</strong>
         <p>ITIL Foundation</p>
-        <p>70-410 - Installing and Configuring Windows Server 2012 R2 </p>
+        <p>MCP (Microsoft Ceritied Professional) in Configuring WIndows Server 2012 R2. Exam code: <b>70-410</b></p>
     </Fragment>
 ]
 
