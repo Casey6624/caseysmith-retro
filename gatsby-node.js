73d842fd1f9462fcdcaa341055f4a3712fa23c21
Casey Smith <caseysmith@Caseys-MBP.lan>
2020-05-16 11:49:29 +0100

    Still broken, switch branch

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,9 +9,20 @@ const getOnlyPublished = edges =>
 exports.createPages = async ({ actions, graphql }) => {
     const { createPage } = actions
 
+    const result = await graphql(`
+      {
+        allWordpressPost {
+          edges {
+            node {
+              id
+              slug
+              status
+            }
+          }
+        }
+      }
 
-    let graphQLPageQuery = await graphql(`
-    {
+      {
         allWordpressPage{
         edges{
           node{
@@ -28,57 +39,63 @@ exports.createPages = async ({ actions, graphql }) => {
         }
       }
     }
-    `)
-
-    console.log( await graphQLPageQuery.data)
-
-    throw new Error('fuckk hahaha')
-
-    return graphql(`
-      {
-        allWordpressPost {
-          edges {
-            node {
-              id
-              slug
-              status
-            }
-          }
-        }
-      }
       `)
-        .then(result => {
-            if (result.errors) {
-                result.errors.forEach(e => console.error(e.toString()))
-                return Promise.reject(result.errors)
-            }
 
+
+      if(result.errors){
+        console.log(result.errors)
+        return Promise.reject(result.errors)
+      }
+
+            const portfolioTemplate = path.resolve(`./src/templates/portfolio.js`)
+            const projectTemplate = path.resolve(`./src/templates/project.js`)
             const postTemplate = path.resolve(`./src/templates/post.js`)
             const postsTemplate = path.resolve(`./src/templates/posts.js`)
 
-            // In production builds, filter for only published posts.
-            const allPosts = result.data.allWordpressPost.edges
-            const posts =
-                process.env.NODE_ENV === 'production'
-                    ? getOnlyPublished(allPosts)
-                    : allPosts
+            const {allWordpressPost, allWordpressPage} = result.data
 
-            // Iterate over the array of posts
-            _.each(posts, ({ node: post }) => {
-                // Create the Gatsby page for this WordPress post
-                createPage({
-                    path: `/thought/${post.slug}/`,
-                    component: postTemplate,
-                    context: {
-                        id: post.id,
-                    },
-                })
-                // Create Posts (thoughts)
-                createPage({
-                    path: `/thoughts/`,
-                    component: postsTemplate
-                });
+            allWordpressPage.edges.forEach(edge => {
+              // Gatsby uses Redux to manage its internal state.
+              // Plugins and sites can use functions like "createPage"
+              // to interact with Gatsby.
+                            // custom
+              createPage({
+                // Each page is required to have a `path` as well
+                // as a template component. The `context` is
+                // optional but is often necessary so the template
+                // can query data specific to each page.
+                path: `/project/${edge.node.slug}/`,
+                component: projectTemplate,
+                context: {
+                  id: edge.node.id,
+                },
+              })
             })
+
+            createPage({
+              path: `/portfolio/`,
+              component: portfolioTemplate
+          });
+
+          allWordpressPost.edges.forEach(edge => {
+            // Gatsby uses Redux to manage its internal state.
+            // Plugins and sites can use functions like "createPage"
+            // to interact with Gatsby.
+                          // custom
+                            // Create the Gatsby page for this WordPress post
+                            createPage({
+                              path: `/thought/${post.slug}/`,
+                              component: postTemplate,
+                              context: {
+                                  id: post.id,
+                              },
+                          })
+                          // Create Posts (thoughts)
+                          createPage({
+                              path: `/thoughts/`,
+                              component: postsTemplate
+                          });
+          })
 
             // Create a paginated blog, e.g., /, /page/2, /page/3
             paginate({
@@ -88,9 +105,6 @@ exports.createPages = async ({ actions, graphql }) => {
                 pathPrefix: ({ pageNumber }) => (pageNumber === 0 ? `/` : `/page`),
                 component: postTemplate,
             })
-        })
-
-
 
 }
 
